acfd4b4dce7db792978084955c624123687fd6b9
Chlopi <chlopi@Alinas-MacBook-Pro.local>
2022-03-21 12:34:31 +0100

    UI for Newsfeed screen has been implemented, bugs are fixed.

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # nyt_viewer_v1
 
-A new Flutter project.
+# Developing:
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+1. Build an Bottom Tap Bar. - DONE
+2. Create a Models from JSON. - DONE
+3. Populate a ListViewBuilder usin FutureBuilde. - DONE
+4. Layout a UI for ListViewItem. - DONE
+5. Format publishedDate property for presenting to user. - DONE
+6. Reorganazing code for the Provider package.
+7. Implement favorites newsfeed.
+8. Implement a Bottom Sheet for detail view.
 
 
 # Debugging:
 
 1. If a server respond without an image - render a default one. DONE
-
-2. If a server respond with no text information (title = null) - render subtitle and the image
-
+2. If a server respond with no text information (title = null) - render subtitle and the image. DONE
 3. If a server respond only with an image (no text information at all) - do not render an object.
+4. CircularProgressIndicator - refactoring didn't work with if(snapshot.hasData) - insted base of (snapshot.hasData) redering ither ListViewBuilder or CircularProgressIndicator. - DONE
+
+
+
+# Additional Features:
+
+1. Responsivnes (Web version).
--- lib/widgets/newsfeed_item.dart
+++ lib/widgets/newsfeed_item.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:nyt_viewer_v1/models/content.dart';
+import 'package:intl/intl.dart';
 
 class NewsfeedItem extends StatelessWidget {
   final List<Result>? listOfResults;
@@ -15,31 +18,73 @@ class NewsfeedItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    final title = listOfResults![index].title;
-    //final bodyText = listOfResults![index].bodyText;
-    return (title != '') ? SizedBox(
-      height: screenSize.height * 0.2,
-      child: Card(
-        child: Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 16),
-              width: screenSize.width * 0.3,
-              child: Image.network(imagePath),
+    String? title;
+    String publicationDate;
+    DateTime? formattedDate;
+    var format = DateFormat.yMMMMEEEEd();
+    var dateString;
+
+    if (listOfResults != null) {
+      title = listOfResults![index].title;
+      
+    }
+    if (listOfResults![index].publishedDate != null) {
+      publicationDate = listOfResults![index].publishedDate!;
+      formattedDate = DateTime.parse(publicationDate);
+      dateString = format.format(formattedDate);
+      
+      }
+    return (title != '')
+        ? SizedBox(
+            height: screenSize.height * 0.2,
+            child: Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    
+                    width: screenSize.width * 0.3,
+                    child: Image.network(imagePath),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          
+                          height: screenSize.height * 0.09,
+                          width: screenSize.width * 0.6,
+                          child: Text(
+                            '$title',
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          width: screenSize.width * 0.6,
+                          
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                
+                                child: Text('$dateString'),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.favorite_outline_rounded, color: Colors.red,),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Column(
-              children: [
-                Container(
-                  color: Colors.red,
-                  height: screenSize.height * 0.09,
-                  width: screenSize.width * 0.6,
-                  child: Text('${listOfResults![index].title}', style: TextStyle(fontSize: 20),)),
-              ],
-            )
-          ],
-        ),
-      ),
-    ) : const SizedBox();
+          )
+        : const SizedBox();
   }
 }
-
